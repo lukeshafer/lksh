33a58629b92e30cb65d2350538033c260fc7242e
Luke Shafer <lukealanshafer@gmail.com>
2022-11-09 00:07:54 -0500

    file cleanup and improve responsive appearance

--- src/components/StackList.ts
+++ src/components/StackList.ts
@@ -1,10 +1,10 @@
-import type { IconTypes } from 'solid-icons'
-import type { HEX } from '../../../utils/colors'
+//import type { IconTypes } from 'solid-icons'
+type HEX = `#${string}`
 
 export interface StackProps {
 	name: string
 	tag: string
-	SolidIcon: IconTypes
+	//SolidIcon: IconTypes
 	c1: HEX
 	c2: HEX
 }
@@ -14,7 +14,7 @@ export const stackList = new Map<string, StackProps>([
 		'React',
 		{
 			name: 'simple-icons:react',
-			SolidIcon: (await import('solid-icons/si')).SiReact,
+			//SolidIcon: (await import('solid-icons/si')).SiReact,
 			tag: 'react',
 			c1: '#7de0fc',
 			c2: '#149eca',
@@ -24,7 +24,7 @@ export const stackList = new Map<string, StackProps>([
 		'TypeScript',
 		{
 			name: 'simple-icons:typescript',
-			SolidIcon: (await import('solid-icons/si')).SiTypescript,
+			//SolidIcon: (await import('solid-icons/si')).SiTypescript,
 			tag: 'typescript',
 			c1: '#3178c6',
 			c2: '#235a97',
@@ -34,7 +34,7 @@ export const stackList = new Map<string, StackProps>([
 		'Astro',
 		{
 			name: 'simple-icons:astro',
-			SolidIcon: (await import('solid-icons/si')).SiAstro,
+			//SolidIcon: (await import('solid-icons/si')).SiAstro,
 			tag: 'react',
 			c1: '#6820a4',
 			c2: '#c026d3',
@@ -44,7 +44,7 @@ export const stackList = new Map<string, StackProps>([
 		'PostCSS',
 		{
 			name: 'simple-icons:postcss',
-			SolidIcon: (await import('solid-icons/si')).SiPostcss,
+			//SolidIcon: (await import('solid-icons/si')).SiPostcss,
 			tag: 'postcss',
 			c1: '#dd3a0a',
 			c2: '#dd3735',
@@ -54,7 +54,7 @@ export const stackList = new Map<string, StackProps>([
 		'Node.js',
 		{
 			name: 'simple-icons:nodedotjs',
-			SolidIcon: (await import('solid-icons/si')).SiNodedotjs,
+			//SolidIcon: (await import('solid-icons/si')).SiNodedotjs,
 			tag: 'nodejs',
 			c1: '#77b65d',
 			c2: '#3e863d',
@@ -64,7 +64,7 @@ export const stackList = new Map<string, StackProps>([
 		'Svelte',
 		{
 			name: 'simple-icons:svelte',
-			SolidIcon: (await import('solid-icons/si')).SiSvelte,
+			//SolidIcon: (await import('solid-icons/si')).SiSvelte,
 			tag: 'svelte',
 			c1: '#ff3e00',
 			c2: '#c25d2e',
@@ -74,7 +74,7 @@ export const stackList = new Map<string, StackProps>([
 		'TailwindCSS',
 		{
 			name: 'simple-icons:tailwindcss',
-			SolidIcon: (await import('solid-icons/si')).SiTailwindcss,
+			//SolidIcon: (await import('solid-icons/si')).SiTailwindcss,
 			tag: 'tailwindcss',
 			c1: '#38bdf8',
 			c2: '#f472b6',
@@ -84,7 +84,7 @@ export const stackList = new Map<string, StackProps>([
 		'Next.js',
 		{
 			name: 'simple-icons:nextdotjs',
-			SolidIcon: (await import('solid-icons/si')).SiNextdotjs,
+			//SolidIcon: (await import('solid-icons/si')).SiNextdotjs,
 			tag: 'nextjs',
 			c1: '#e3e1f6',
 			c2: '#ebeae8',
@@ -94,7 +94,7 @@ export const stackList = new Map<string, StackProps>([
 		'SolidJS',
 		{
 			name: 'simple-icons:solid',
-			SolidIcon: (await import('solid-icons/si')).SiSolid,
+			//SolidIcon: (await import('solid-icons/si')).SiSolid,
 			tag: 'solidjs',
 			c1: '#87bee6',
 			c2: '#3966b1',
--- src/utils/getDate.ts
+++
@@ -1,9 +0,0 @@
-export function getDateStr(dateStr: string, yearOnly = false) {
-	if (dateStr === "") return "Present";
-	const date = new Date(dateStr).toDateString();
-	const [_day, month, _date, year] = date.split(" ");
-
-	if (yearOnly) return year;
-
-	return `${month} ${year}`;
-}
